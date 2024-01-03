78d6ab56070e25a0bc8c64ad56c669a4dad13893
dominic-source <chinonsodomnic@gmail.com>
2024-01-03 21:24:22 +0100

    debug code

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,16 +1,16 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((values) => [
-      {
-        status: values[0].status,
-        value: values[0],
-      },
-      {
-        status: values[1].status,
-        value: values[1],
-      },
-    ]);
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  const values_1 = await Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]);
+    return [
+        {
+            status: values_1[0].status,
+            value: values_1[0].status === 'fulfilled' ? values_1[0].value : values_1[0].reason,
+        },
+        {
+            status: values_1[1].status,
+            value: values_1[1].status === 'fulfilled' ? values_1[1].value : values_1[1].reason,
+        },
+    ];
 }

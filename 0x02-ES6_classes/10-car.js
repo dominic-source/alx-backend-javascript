b167c870464bdc6aab50c3e4cef14314cde54168
dominic-source <chinonsodomnic@gmail.com>
2024-01-04 19:54:13 +0100

    learn about symbol species

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,18 @@
+export default class Car {
+  constructor(brand, motor, color) {
+    this._brand = undefined;
+    this._motor = undefined;
+    this._color = undefined;
+    if (typeof brand === 'string') this._brand = brand;
+    if (typeof motor === 'string') this._motor = motor;
+    if (typeof color === 'string') this._color = color;
+  }
+
+  static get [Symbol.species]() {
+    return this;
+  }
+
+  cloneCar() {
+    return new this.constructor();
+  }
+}

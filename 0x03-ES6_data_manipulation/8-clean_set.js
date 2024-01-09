d3358f50ad81949bdcf346e3ceadf7cd744b19b9
dominic-source <chinonsodomnic@gmail.com>
2024-01-09 20:35:35 +0100

    clean set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,7 @@
 export default function cleanSet(set, sString) {
   let newString = '';
   for (const str of set) {
-    if (str.startsWith(sString) && sString) {
+    if (str.startsWith(sString) && sString.length !== 0) {
       newString += str.slice((sString.length));
       newString += '-';
     }

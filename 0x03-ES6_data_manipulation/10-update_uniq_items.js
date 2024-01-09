bf85b8fdb5e17e66662a6c43686d41609d57532b
dominic-source <chinonsodomnic@gmail.com>
2024-01-09 21:15:20 +0100

    update unique entries

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,13 +1,12 @@
 export default function updateUniqueItems(myMap) {
-  for (const [key, value] of myMap) {
-    if (value === 1) {
-      try {
+  try {
+    for (const [key, value] of myMap.enteries()) {
+      if (value === 1) {
         myMap.set(key, 100);
-      } catch (error) {
-        throw new Error('Cannot process');
       }
     }
+  } catch (error) {
+    throw new Error('Cannot process');
   }
-
   return myMap;
 }

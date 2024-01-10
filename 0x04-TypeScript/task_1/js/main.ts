f91703a1f2ba1fbb86ee8831d253ede30d9ca126
dominic-source <chinonsodomnic@gmail.com>
2024-01-10 17:26:58 +0100

    task 4

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -10,4 +10,11 @@ interface Teacher{
 interface Directors extends Teacher {
     numberOfReports:number;
 }
-  
+
+export function printTeacher(firstName: string, lastName: string): string {
+    return `${firstName[0]}. ${lastName}`;
+}
+
+interface printTeacherFunction {
+    (firstName: string, lastName: string): string;   
+}

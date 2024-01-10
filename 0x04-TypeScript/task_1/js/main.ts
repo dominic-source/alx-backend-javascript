fb14182abb2701160f4c57bb31862066c0df9342
dominic-source <chinonsodomnic@gmail.com>
2024-01-10 18:32:07 +0100

    task 5 completed

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -18,3 +18,26 @@ export function printTeacher(firstName: string, lastName: string): string {
 interface printTeacherFunction {
     (firstName: string, lastName: string): string;   
 }
+
+interface student {
+    firstName: string;
+    lastName: string;
+    workOnHomework(): string;
+    displayName(): string;
+}
+
+class StudentClass implements student{
+    firstName: string;
+    lastName: string;
+    constructor(firstName: string, lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    workOnHomework() {
+        return 'Currently working';
+    }
+    displayName() {
+        return this.firstName;
+    }
+}

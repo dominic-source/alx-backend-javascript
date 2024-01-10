29b7487557aaa37eedee8d370f6abb2db385677a
dominic-source <chinonsodomnic@gmail.com>
2024-01-10 10:17:53 +0100

    typed array usage

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -2,9 +2,9 @@ export default function createInt8TypedArray(length, position, value) {
   const buffer = new ArrayBuffer(length);
   let unit;
   try {
-    unit = new Int8Array(buffer);
-    unit[position] = value;
+    unit = new DataView(buffer);
+    unit.setUint8(position, value);
   } catch (error) { throw new Error('Position outside range'); }
 
-  return buffer;
+  return unit;
 }

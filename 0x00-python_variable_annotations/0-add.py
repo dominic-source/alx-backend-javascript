b338d8fced7811979e01df828cff45fb4f5721cd
dominic-source <chinonsodomnic@gmail.com>
2024-01-11 20:09:40 +0100

    manage javascript

--- 0x00-python_variable_annotations/0-add.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-"""This module using python type annotation to add values"""
-
-def add(a: float, b: float) -> float:
-    """This function will add two floating point number 
-    and return a float"""
-
-    return a + b
--- 0x00-python_variable_annotations/README.md
+++
@@ -1,3 +0,0 @@
-# PYTHON VARIABLE ANNOTATION
-
-> In this python tasks and problems, I solve some of the mysteries behind python annotation in the task

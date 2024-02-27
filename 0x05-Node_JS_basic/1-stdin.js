2f274465c1b32187faf205e954bbf11c80bd1fd1
dominic-source <chinonsodomnic@gmail.com>
2024-02-27 14:46:51 +0100

    update code

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,10 +1,10 @@
-const process = require('node:process');
+const process = require('process');
 
 const text = 'Welcome to Holberton School, what is your name?';
 process.stdout.write(`${text}\n`);
 
-process.stdin.on('readable', () => {
-  const INPUT = process.stdin.read();
+process.stdin.on('data', (input) => {
+  const INPUT = input.toString().trim();
   process.stdout.write(`Your name is: ${INPUT}`);
   process.stdout.write('This important software is now closing\n');
   process.exit();

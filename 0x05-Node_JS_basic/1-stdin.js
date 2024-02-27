fb7ee4d1ad83ea8c69e17986809779bceb8d0445
dominic-source <chinonsodomnic@gmail.com>
2024-02-27 15:29:19 +0100

    update stdin v6

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -6,8 +6,11 @@ process.stdout.write(`${text}`);
 process.stdin.on('data', (input) => {
   const INPUT = input.toString().trim();
   process.stdout.write(`Your name is: ${INPUT}\n`);
-  // if (!process.stdin.isTTY) {
+  if (process.stdin.isTTY) {
+    process.exit();
+  }
+});
+
+process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
-  // }
-  process.exit();
 });

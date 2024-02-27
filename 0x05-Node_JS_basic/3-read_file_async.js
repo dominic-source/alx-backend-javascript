cbb0dc1716a7e447f6e52328585a25b77551c4db
dominic-source <chinonsodomnic@gmail.com>
2024-02-27 21:39:12 +0100

    create readfile async function

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,43 @@
+const fs = require('fs');
+const readline = require('readline');
+
+module.exports = async function countStudents(path) {
+  const promise = new Promise((resolve, reject) => {
+    const stream = fs.createReadStream(path, 'utf-8');
+    const rl = readline.createInterface({
+      input: stream,
+      crlfDelay: Infinity,
+    });
+    let count = -1;
+    const fields = {};
+    // Readline
+    rl.on('line', (line) => {
+      const struct = line.toString().trim();
+      if (struct !== '') {
+        count += 1;
+        const info = struct.split(',');
+        try {
+          fields[info[3]].push(info[0]);
+        } catch (err) {
+          fields[info[3]] = [];
+          fields[info[3]].push(info[0]);
+        }
+      }
+    });
+
+    rl.on('error', () => {
+      reject(new Error('Cannot load the database'));
+    });
+
+    rl.on('close', () => {
+      delete fields.field;
+      console.log('Number of students:', count);
+      const keys = Object.keys(fields);
+      for (const dat of keys) {
+        console.log(`Number of students in ${dat}: ${fields[dat].length}. List: ${fields[dat].join(', ')}`);
+      }
+      resolve();
+    });
+  });
+  return promise;
+};

fe2ffec723224fe758dee1bff1d278bf36c2b31b
dominic-source <chinonsodomnic@gmail.com>
2024-02-28 08:24:12 +0100

    update

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -7,5 +7,6 @@ process.stdin.on('data', (INPUT) => {
   process.stdout.write(`Your name is: ${INPUT}`);
 });
 process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
   process.exit();
 });

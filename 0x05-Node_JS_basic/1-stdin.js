d1d293df388de4ef7e696caa91e31bffff2b84b0
dominic-source <chinonsodomnic@gmail.com>
2024-02-28 08:13:06 +0100

    stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -3,15 +3,14 @@ const process = require('process');
 const text = 'Welcome to Holberton School, what is your name?\n';
 process.stdout.write(`${text}`);
 
-process.stdin.on('data', (input) => {
-  const INPUT = input.toString().trim();
+process.stdin.on('data', (INPUT) => {
   process.stdout.write(`Your name is: ${INPUT}`);
   if (!process.stdin.isTTY) {
-    console.log();
+    // console.log();
   } else {
     process.exit();
   }
 });
-process.stdin.on('end', () => {
-  process.stdout.write('This important software is now closing\n');
-});
+// process.stdin.on('end', () => {
+//   process.stdout.write('This important software is now closing\n');
+// });

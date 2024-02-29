3c5092bf43f2943017fd47248e68021840ee38d6
dominic-source <chinonsodomnic@gmail.com>
2024-02-29 16:13:25 +0100

    run test for addition, subtraction, and division v12

--- 0x06-unittests_in_js/1-calcul.js
+++ 0x06-unittests_in_js/1-calcul.js
@@ -2,7 +2,7 @@ module.exports = function calculateNumber(type, a, b) {
   const na = Math.round(a);
   const nb = Math.round(b);
   if (type === 'SUM') return na + nb;
-  if (type === 'SUBTRACT') return nb - na;
+  if (type === 'SUBTRACT') return na - nb;
   if (type === 'DIVIDE') {
     if (nb === 0) return 'Error';
     return na / nb;

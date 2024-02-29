3d6f52600c5ac346ba97b2e3ab5124c41888a984
dominic-source <chinonsodomnic@gmail.com>
2024-03-01 00:05:33 +0100

    create integration testing of application v5

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -4,35 +4,39 @@ const request = require('request');
 
 describe('Index page', function() {
 
-  it('should have the correct status code', function() {
+  it('should have the correct status code', function(done) {
     request('http://localhost:7865/', (error, response, body) => {
       if (!error) {
         expect(response.statusCode).to.equal(200);
+        done();
       }
     })
   });
 
-  it('should have the correct message body', function() {
+  it('should have the correct message body', function(done) {
     request('http://localhost:7865/', (error, response, body) => {
       if (!error) {
         expect(body).to.equal('Welcome to the payment system');
+        done();
       }
     })
   });
 
   
-  it('should have the correct status message', function() {
+  it('should have the correct status message', function(done) {
     request('http://localhost:7865/', (error, response, body) => {
       if (!error) {
         expect(response.statusMessage).to.equal('OK');
+        done();
       }
     })
   });
 
-  it('should have the correct method', function() {
+  it('should have the correct method', function(done) {
     request('http://localhost:7865/', (error, response, body) => {
       if (!error) {
         expect(response.headers['content-length']).to.equal('29');
+        done();
       }
     })
   });

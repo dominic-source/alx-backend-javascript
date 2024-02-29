24430c879ab7aaf5e7c05b1e72c32b681f19957d
dominic-source <chinonsodomnic@gmail.com>
2024-02-29 13:15:38 +0100

    run test on calculate number

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,33 +1,29 @@
-const chai = require('chai');
-// const  expect = chai.expect;
-// const sinon = require('sinon');
 const assert = require('assert');
-const calc = require('./0-calcul.js');
-
+const  calculateNumber = require('./0-calcul.js');
 
 describe('calculateSum', function() {
   it('should return the  correct sum when add two positive numbers', function() {
-    const val = calc.calculateNumber(2, 5.5);
-    assert(val, 8);
+    const val = calculateNumber(2, 5.5);
+    assert.strictEqual(val, 8);
   });
 
-  it('should return the sum of two numbers', function() {
-    const val = calc.calculateNumber(3.5, 2.1);
-    assert(val, 6);
+  it('should return the the correct sum when adding two negative decimal numbers', function() {
+    const val = calculateNumber(-3.5, -2.1);
+    assert.strictEqual(val, -5);
   });
 
-  it('should return the sum of two numbers', function() {
-    const val = calc.calculateNumber(5.1, 2.1);
-    assert(val, 7);
+  it('should return the correct sum when adding a positive and a negative decimal numbers', function() {
+    const val = calculateNumber(5.1, -2.1);
+    assert.strictEqual(val, 3);
   });
 
-  it('should return the sum of two numbers', function() {
-    const val = calc.calculateNumber(1.2, 3.5);
-    assert(val, 5);
+  it('should return the correct sum when adding two decimal numbers', function() {
+    const val = calculateNumber(1.2, 3.5);
+    assert.strictEqual(val, 5);
   });
 
-  it('should return the sum of two numbers', function() {
-    const val = calc.calculateNumber(2, 3);
-    assert(val, 5);
+  it('should return the correct sum when adding two integer dumbers', function() {
+    const val = calculateNumber(2, 3);
+    assert.strictEqual(val, 5);
   });
 });

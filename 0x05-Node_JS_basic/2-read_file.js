e7e06ccdd37e57262e8aaa860843e80a28c3ddb6
dominic-source <chinonsodomnic@gmail.com>
2024-03-01 11:40:47 +0100

    read file

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,34 +1,33 @@
 const fs = require('fs');
 
 function countStudents(path) {
-  let data = null;
   try {
-    data = fs.readFileSync(path, 'utf-8');
-  } catch (err) {
-    throw new Error('Cannot load the database');
-  }
+    const data = fs.readFileSync(path, 'utf-8');
 
-  const results = data.split('\r\n');
+    const results = data.split('\r\n');
 
-  const fields = {};
-  let count = -1;
-  for (const struct of results) {
-    if (struct !== '') {
-      count += 1;
-      const info = struct.split(',');
-      try {
-        fields[info[3]].push(info[0]);
-      } catch (err) {
-        fields[info[3]] = [];
-        fields[info[3]].push(info[0]);
+    const fields = {};
+    let count = -1;
+    for (const struct of results) {
+      if (struct !== '') {
+        count += 1;
+        const info = struct.split(',');
+        try {
+          fields[info[3]].push(info[0]);
+        } catch (err) {
+          fields[info[3]] = [];
+          fields[info[3]].push(info[0]);
+        }
       }
     }
-  }
-  delete fields.field;
-  console.log(`Number of students: ${count}`);
-  const keys = Object.keys(fields);
-  for (const dat of keys) {
-    console.log(`Number of students in ${dat}: ${fields[dat].length}. List: ${fields[dat].join(', ')}`);
+    delete fields.field;
+    console.log(`Number of students: ${count}`);
+    const keys = Object.keys(fields);
+    for (const dat of keys) {
+      console.log(`Number of students in ${dat}: ${fields[dat].length}. List: ${fields[dat].join(', ')}`);
+    }
+  } catch (err) {
+    throw new Error('Cannot load the database');
   }
 }
 module.exports = countStudents;

43e749449dcaaee29d4b3c2d813090d82e13940b
dominic-source <chinonsodomnic@gmail.com>
2024-03-02 00:08:52 +0100

    add router v2

--- 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -1,35 +1,35 @@
-import readDatabase from "../utils";
+import readDatabase from '../utils';
 
 class StudentsController {
   static getAllStudents(request, response) {
     let [file] = process.argv.slice(2);
     file = file || 'nofile';
 
-    readDatabase(file).then(val => {
+    readDatabase(file).then((val) => {
       let info = 'This is the list of our students\n';
-      const obj_k = Object.keys(val);
-      for (const key of obj_k) {
-        info += `Number of students in ${key}: ${val[key].length}. List: ${val[key].join(', ')}\n`
+      const objK = Object.keys(val);
+      for (const key of objK) {
+        info += `Number of students in ${key}: ${val[key].length}. List: ${val[key].join(', ')}\n`;
       }
-      response.status(200).send(info);
-    }).catch((err) => {
+      response.status(200).send(`<pre>${info}</pre>`);
+    }).catch(() => {
       response.status(500).send('Cannot load the database');
     });
   }
 
   static getAllStudentsByMajor(request, response) {
-    const mj = request.params['major'];
+    const mj = request.params.major;
 
     if (mj === 'CS' || mj === 'SWE') {
       let [file] = process.argv.slice(2);
       file = file || 'nofile';
 
-    readDatabase(file).then(val => {
-      const info = `List: ${val[mj].join(', ')}\n`;
-      response.status(200).send(info);
-    }).catch((err) => {
-      response.status(500).send('Cannot load the database');
-    });
+      readDatabase(file).then((val) => {
+        const info = `List: ${val[mj].join(', ')}\n`;
+        response.status(200).send(`<pre>${info}</pre>`);
+      }).catch(() => {
+        response.status(500).send('Cannot load the database');
+      });
     } else {
       response.status(500).send('Major parameter must be CS or SWE');
     }
